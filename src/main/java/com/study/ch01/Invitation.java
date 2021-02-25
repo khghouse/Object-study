0a5daf2d69649648e5d177a8e8a57d7e3d369492
khghouse <khghoue@gmail.com>
2021-02-25 20:34:27 +0900

    ch01 p18 before refactoring

---
+++ src/main/java/com/study/ch01/Invitation.java
@@ -0,0 +1,7 @@
+package com.study.ch01;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+	private LocalDateTime when; // 초대일자
+}
---
+++ src/main/java/com/study/ch01/Ticket.java
@@ -0,0 +1,9 @@
+package com.study.ch01;
+
+public class Ticket {
+	private Long fee;
+
+	public Long getFee() {
+		return fee;
+	}
+}
